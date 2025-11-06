b0cd1398d9a7280fa0b211bb6b19c1df6e396547
The insightful solutions <theinsightfulsolutions15@gmail.com>
2025-11-06 10:50:32 +0000

    search ka pta ni chl rha screan ko shi se adjust kre

--- src/app/dashboard/movement/page.tsx
+++ src/app/dashboard/movement/page.tsx
@@ -342,7 +342,7 @@ export default function MovementPage() {
                             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                             </Button>
                         </PopoverTrigger>
-                        <PopoverContent className="w-[--radix-popover-trigger-width] p-0">
+                        <PopoverContent className="w-[--radix-popover-trigger-width] p-0" sideOffset={5}>
                             <Command>
                             <CommandInput placeholder="Search animal..." />
                             <CommandEmpty>
